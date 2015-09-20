3f02a74de586bc9a9abca2abfd0d7befb55b31a2
CarsonBills <CarsonMBills@Gmail.com>
2015-09-20 12:25:35 -0400

    recursion

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,8 +1,29 @@
 function factorial(num){
-	var result = num;
-	for(x = num - 1; x > 0; x--){
-		result *= x
-		console.log(result);
+	var result = 1;
+	for (x = 2; x <= num; x++){
+		result *= x;
 	}
 	return result;
+}
+
+function gCD(first, second){
+	var fDiv = [];
+	var sDiv = [];
+	for (x = 1; x < (first/2); x++){
+		if (first % x === 0){
+			fDiv.push(x);
+		}
+	}
+	for (y = 1; y < (second/2); y++){
+		if (second % y === 0){
+			sDiv.push(y);
+		}
+	}
+}
+
+function gcd(a,b){ 
+	if (b == 0)
+   {return a}
+ 	else
+   {return gcd(b, a % b)}
 }
