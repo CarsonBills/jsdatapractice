53813cdcdca0a2a24cc8274df159325addc5fbbe
CarsonBills <CarsonMBills@Gmail.com>
2015-09-21 08:28:40 -0400

    actually uses recursion

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,29 +1,7 @@
 function factorial(num){
-	var result = 1;
-	for (x = 2; x <= num; x++){
-		result *= x;
+	if (num === 0){
+		return 1;
+	} else {
+		return num * factorial(num - 1);
 	}
-	return result;
-}
-
-function gCD(first, second){
-	var fDiv = [];
-	var sDiv = [];
-	for (x = 1; x < (first/2); x++){
-		if (first % x === 0){
-			fDiv.push(x);
-		}
-	}
-	for (y = 1; y < (second/2); y++){
-		if (second % y === 0){
-			sDiv.push(y);
-		}
-	}
-}
-
-function gcd(a,b){ 
-	if (b == 0)
-   {return a}
- 	else
-   {return gcd(b, a % b)}
 }
