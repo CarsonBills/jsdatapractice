d98d0a8a155a92bfdb82442281f1819be43cb350
CarsonBills <CarsonMBills@Gmail.com>
2015-10-21 17:00:30 -0400

    quicksort

--- sorting.js
+++ sorting.js
@@ -267,3 +267,20 @@ function insertSort(arr){ // least to greatest
 	}
 	return arr
 }
+
+function quickSort(arr){
+	if (arr.length === 0){
+		return []
+	}
+	var pivot = arr[0];
+	var left = [];
+	var right = [];
+	for (var i = 1; i < arr.length; i++){
+		if (arr[i] < pivot){
+			left.push(arr[i]);
+		} else {
+			right.push(arr[i]);
+		}
+	}
+	return quickSort(left).concat(pivot, quickSort(right));
+}
